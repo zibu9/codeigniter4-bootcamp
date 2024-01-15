fab66321567ccdeb8f29e5eb570070bbc27934c7
Zibu <58338897+zibu9@users.noreply.github.com>
2024-01-15 19:53:13 +0100

    add pages controllers

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Controllers\Pages;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/shop', 'Shop::index');
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
